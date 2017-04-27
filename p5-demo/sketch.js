6f1b1a46da7ecf3231a9519c9b0cee6c71c63e9d
Teresa Lamb <flour1337@gmail.com>
2017-04-26 21:45:37 -0400

    now includes sketch from class

--- p5-demo/sketch.js
+++ p5-demo/sketch.js
@@ -1,6 +1,23 @@
+var party = 60;
+var r = 30;
+var g = 100;
+var b = 200;
 function setup() {
+
+  //changed background color to black
 
 }
 function draw() {
+  //make my canvas
+  createCanvas(windowWidth, windowHeight);
+  if(mouseIsPressed){
+    background('#ff9999');
+  } else {
+    background('#9999ff');
+  }
 
+  fill(r, mouseY, b);
+  ellipse(mouseX, mouseY, party, party);
+  rect(200, 200, party*2, party*3);
+  line(mouseX, mouseY, mouseX + 100, mouseY + 100);
 }
